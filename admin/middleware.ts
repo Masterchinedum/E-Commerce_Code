787c6986b5cccd0bc7bb37d8943cb9df166ce5c9
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-27 03:48:21 +0000

    chore: Update @clerk/nextjs import to use server module in middleware

--- admin/middleware.ts
+++ admin/middleware.ts
@@ -1,9 +1,39 @@
-import { authMiddleware } from "@clerk/nextjs";
+// import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  publicRoutes: ["/api/:path*"],
+// export default authMiddleware({
+//   publicRoutes: ["/api/:path*"],
+// });
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware({
+//   publicRoutes: ["/api/:path*"],
+// });
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isApiRoute = createRouteMatcher(['/api/:path*']);
+
+export default clerkMiddleware((auth, req) => {
+  if (!isApiRoute(req)) {
+    auth().protect();
+  }
 });
 
+
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
